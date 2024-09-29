ae80fc7e6a6737d4fb38c48ac6a960c71dc4b440
김예원 <yaewon4643@naver.com>
2024-09-30 02:50:02 +0900

    [ISSUE 11] feat: Order Management BE

--- src/main/java/com/ureca/myspring/dto/QuestionDTO.java
+++ src/main/java/com/ureca/myspring/dto/QuestionDTO.java
@@ -1,6 +1,5 @@
 package com.ureca.myspring.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -20,24 +19,21 @@ import lombok.NoArgsConstructor;
 public class QuestionDTO {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private long id;
 	
 	@Column(name="store_id")
-	private int storeID; // 해당 질문이 속한 폼 번호 (폼 번호를 알면 storeId도 알 수 있음)
+	private long storeID; // 해당 질문이 속한 폼 번호 (폼 번호를 알면 storeId도 알 수 있음)
 	
 	@Column(name="question_text")
-	@JsonProperty("question_text")
 	private String questionText; // 질문
 	
 	@Column(name="question_type")
-	@JsonProperty("question_type")
 	private String questionType; // 입력 형식 (텍스트, 드롭다운, 날짜, 파일 업로드)
 	
 	@Column(name="is_active")
-	private int isActive; // 질문 활성화 여부 (삭제 시 비활성화)
+	private long isActive; // 질문 활성화 여부 (삭제 시 비활성화)
 	
 	@Column(name = "question_content", columnDefinition = "JSON")
-    @JsonProperty("question_content")
     private String questionContent; // JSON 형식으로 배열을 저장하는 필드
 	
 }	
